9ec476af7ff6cdd24308e5ac15fc0f5bc82c3d49
shailove13h <enlightenmentwithsv@gmail.com>
2023-04-11 22:21:57 +0530

    db update

--- form/settings.py
+++ form/settings.py
@@ -87,27 +87,27 @@ WSGI_APPLICATION = 'form.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases  freeforms
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES = {
+#         'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'freeforms',
+#         'NAME': 'freeforms',
 
-        'USER': 'postgres',
+#         'USER': 'postgres',
 
-        'PASSWORD': '123',
+#         'PASSWORD': '123',
 
-        'HOST': 'localhost',
+#         'HOST': 'localhost',
 
-        'PORT': '5432',
-        }
-     }
+#         'PORT': '5432',
+#         }
+#      }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
